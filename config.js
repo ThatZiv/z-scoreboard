9a2f44e9084c7ff1d117fbf6aa095e34cc1eb185
ThatZiv <35277113+ThatZiv@users.noreply.github.com>
2018-07-20 15:43:30 -0400

    V.2

---
+++ config.js
@@ -0,0 +1,9 @@
+/* Config 2 */
+var title = 'Test Server';
+var footer = 'discord.gg/xyz';
+/* ------------------------------- */
+/* ext no touchy touchy */
+document.getElementById('title').innerHTML = title;
+document.getElementById('footer').innerHTML = footer;
+
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
